d20dc4b3b3fbdfcee257ba8038f196981503cf00
motiveko <rhehdrla@naver.com>
2025-02-28 13:13:17 +0900

    fix(jest-prests): jest에서 tsconfig path alias 동작하게 변경

--- packages/jest-presets/node/jest-preset.mjs
+++ packages/jest-presets/node/jest-preset.mjs
@@ -1,3 +1,11 @@
+import path from "path";
+import fs from "fs";
+import { pathsToModuleNameMapper } from "ts-jest";
+
+// 현재 작업 디렉토리(cwd) 기준으로 tsconfig.json 불러오기
+const tsconfigPath = path.join(process.cwd(), "tsconfig.json");
+const tsconfig = JSON.parse(fs.readFileSync(tsconfigPath, "utf8"));
+
 /** @type {import('jest').Config} */
 const config = {
   roots: ["<rootDir>"],
@@ -11,6 +19,9 @@ const config = {
     "<rootDir>/dist",
   ],
   preset: "ts-jest",
+  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
+    prefix: "<rootDir>/",
+  }),
 };
 
 export default config;
