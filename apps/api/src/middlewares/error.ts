05bad9ad7c476a8cb49bbfca4b9c875a1a16475c
motiveko <rhehdrla@naver.com>
2025-03-01 16:43:18 +0900

    feat(api): error middleware 로깅코드 추가

--- apps/api/src/middlewares/error.ts
+++ apps/api/src/middlewares/error.ts
@@ -2,9 +2,9 @@ import { NotFoundError } from "@api/errors/not-found";
 import { ErrorRequestHandler } from "express";
 
 export const errorMiddleware: ErrorRequestHandler = (err, req, res, next) => {
+  console.error(err.message);
   if (err instanceof NotFoundError) {
     return res.status(err.status).json({ error: err.message });
   }
-
   res.status(500).json({ error: "Internal Server Error" });
 };
