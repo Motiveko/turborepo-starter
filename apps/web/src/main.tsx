27a6fe06d5d376f7435931aa190376118fadfb7d
motiveko <rhehdrla@naver.com>
2025-03-10 22:09:32 +0900

    refactor(web): 불필요 파일 삭제

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import App from "@web/app";
 import "@web/index.css";
 import AppRoutes from "@web/app/routes";
 
