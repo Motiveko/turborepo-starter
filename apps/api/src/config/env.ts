be83b63e028bc8b6b2e34a3eaa3f8535961ce628
motiveko <rhehdrla@naver.com>
2025-04-04 18:46:06 +0900

    feat(api): 환경변수 assertion 추가

--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -3,18 +3,35 @@ import * as dotenv from "dotenv";
 const env = process.env.NODE_ENV || "development";
 dotenv.config({ path: `.env.${env}` });
 
+function getEnvVariable(key: string, required?: true): string;
+function getEnvVariable(key: string, required?: false): string | undefined;
+function getEnvVariable(key: string, required = true): string | undefined {
+  const value = process.env[key];
+  if (required && value === undefined) {
+    throw new Error(`Missing environment variable: ${key}`);
+  }
+
+  return value;
+}
+
 export const Config = {
-  SERVICE_NAME: process.env.SERVICE_NAME,
-  PORT: process.env.PORT,
-  APPLICATION_LOG_DIR: process.env.APPLICATION_LOG_DIR,
-  TYPEORM_CONNECTION: process.env.TYPEORM_CONNECTION,
-  TYPEORM_HOST: process.env.TYPEORM_HOST,
-  TYPEORM_USERNAME: process.env.TYPEORM_USERNAME,
-  TYPEORM_PASSWORD: process.env.TYPEORM_PASSWORD,
-  TYPEORM_DATABASE: process.env.TYPEORM_DATABASE,
-  TYPEORM_SCHEMA: process.env.TYPEORM_SCHEMA,
-  TYPEORM_PORT: process.env.TYPEORM_PORT,
-  TYPEORM_SYNCHRONIZE: process.env.TYPEORM_SYNCHRONIZE,
-  TYPEORM_LOGGING: process.env.TYPEORM_LOGGING,
-  TYPEORM_ENTITIES: process.env.TYPEORM_ENTITIES,
+  SERVICE_NAME: getEnvVariable("SERVICE_NAME"),
+  PORT: getEnvVariable("PORT"),
+  APPLICATION_LOG_DIR: getEnvVariable("APPLICATION_LOG_DIR", false),
+  TYPEORM_CONNECTION: getEnvVariable("TYPEORM_CONNECTION"),
+  TYPEORM_HOST: getEnvVariable("TYPEORM_HOST"),
+  TYPEORM_USERNAME: getEnvVariable("TYPEORM_USERNAME"),
+  TYPEORM_PASSWORD: getEnvVariable("TYPEORM_PASSWORD"),
+  TYPEORM_DATABASE: getEnvVariable("TYPEORM_DATABASE"),
+  TYPEORM_SCHEMA: getEnvVariable("TYPEORM_SCHEMA"),
+  TYPEORM_PORT: getEnvVariable("TYPEORM_PORT"),
+  TYPEORM_SYNCHRONIZE: getEnvVariable("TYPEORM_SYNCHRONIZE"),
+  TYPEORM_LOGGING: getEnvVariable("TYPEORM_LOGGING"),
+  TYPEORM_ENTITIES: getEnvVariable("TYPEORM_ENTITIES"),
+  GOOGLE_CLIENT_ID: getEnvVariable("GOOGLE_CLIENT_ID"),
+  GOOGLE_CLIENT_SECRET: getEnvVariable("GOOGLE_CLIENT_SECRET"),
+  GOOGLE_CALLBACK_URL: getEnvVariable("GOOGLE_CALLBACK_URL"),
+  JWT_SECRET: getEnvVariable("JWT_SECRET"),
+  SESSION_SECRET: getEnvVariable("SESSION_SECRET"),
+  FRONTEND_URL: getEnvVariable("FRONTEND_URL"),
 };
