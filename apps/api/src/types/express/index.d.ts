4b41ca847bbaa3b454b636a780d8e600a7ee9a2c
motiveko <rhehdrla@naver.com>
2025-04-11 11:03:46 +0900

    refactor(api): express type 정의 변경

--- apps/api/src/types/express/index.d.ts
+++ apps/api/src/types/express/index.d.ts
@@ -1,4 +1,15 @@
+/* eslint-disable @typescript-eslint/no-empty-interface, @typescript-eslint/no-explicit-any -- for extends type */
 import type { NextFunction, Request, Response } from "express";
+import type { User as CustomUser } from "@api/entities/user";
+
+// https://www.typescriptlang.org/docs/handbook/declaration-files/templates/global-modifying-module-d-ts.html
+// 파일 명이 반드시 index.d.ts 여야함. 어떤걸 변경하는지는 directory명으로 구분한다.
+// e.g. express.d.ts 로 하면 에러가 발생함
+declare global {
+  namespace Express {
+    interface User extends CustomUser {}
+  }
+}
 
 interface MessageResponse {
   message: string;
