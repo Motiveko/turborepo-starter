ee2b1e6949f852e9cadcf6529676bbf639e41f8f
motiveko <rhehdrla@naver.com>
2025-04-15 16:23:07 +0900

    feat(web): react icon 추가 및 버튼반영

--- apps/web/src/features/login/google-login-button.tsx
+++ apps/web/src/features/login/google-login-button.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@web/components/ui/button";
 import { Config } from "@web/config/env";
+import { FcGoogle } from "react-icons/fc";
 
 // TODO : ui 패키지에서 구현
 function GoogleLoginButton() {
@@ -7,7 +8,12 @@ function GoogleLoginButton() {
     window.location.href = `${Config.API_URL}/api/v1/auth/google`;
   };
 
-  return <Button onClick={handleLogin}>Google Login</Button>;
+  return (
+    <Button onClick={handleLogin}>
+      <FcGoogle />
+      Google Login
+    </Button>
+  );
 }
 
 export default GoogleLoginButton;
