8879a6e8b7a4743d460a51f4df7efb22e0ae0995
motiveko <rhehdrla@naver.com>
2025-04-17 21:23:42 +0900

    fix(api): controller 어노테이션에서 에러 케치 오류 수정

--- apps/api/src/decorators/controller.ts
+++ apps/api/src/decorators/controller.ts
@@ -1,14 +1,17 @@
-import type { NextFunction, RequestHandler } from "express";
+import type { NextFunction, RequestHandler, Request, Response } from "express";
 
 /** request handler(controller)에서 에러 발생시 next()로 전달해서 에러 핸들러로 보내질 수 있도록 하는 wrapper function */
 const asyncHandler = (fn: RequestHandler): RequestHandler => {
-  return (req: any, res: any, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
+  return (req: Request, res: Response, next: NextFunction) => {
+    // eslint-disable-next-line @typescript-eslint/no-confusing-void-expression -- TODO : void 표현식 처리 방법 찾기(그냥 promise 래핑하는걸 고려)
+    Promise.resolve(fn(req, res, next)).catch((e) => {
+      next(e);
+    });
   };
 };
 
 export function Controller() {
-  return function (constructor: Function) {
+  return function wrapController(constructor: Function) {
     const methodNames = Object.getOwnPropertyNames(
       constructor.prototype
     ).filter(
