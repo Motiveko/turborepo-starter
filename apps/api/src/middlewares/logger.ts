df9a9f74789f953f2cd0b6c8984c92b22504977f
motiveko <rhehdrla@naver.com>
2025-04-17 21:42:12 +0900

    feat(api): logger에서 response body 도 로깅하도록 기능추가

--- apps/api/src/middlewares/logger.ts
+++ apps/api/src/middlewares/logger.ts
@@ -4,6 +4,17 @@ import { startTimer } from "@api/lib/timer";
 
 export const loggerMiddleware: RequestHandler = (req, res, next) => {
   const getExecutionTime = startTimer();
+  // Store original res.send
+  const originalSend = res.send;
+  let responseBody: any = null;
+
+  // Override res.send
+  res.send = (body): typeof res => {
+    responseBody = body;
+    res.send = originalSend;
+    return originalSend.call(res, body);
+  };
+
   res.on("error", (args) => {
     // console.error(args);
     logger.error(args);
@@ -12,7 +23,8 @@ export const loggerMiddleware: RequestHandler = (req, res, next) => {
   res.on("finish", () => {
     const duration = getExecutionTime();
     const { method, url, protocol, hostname } = req;
-    const { statusCode } = res;
+    const { statusCode } = res; // Remove body from here
+
     logger.info({
       rq: {
         method,
@@ -23,6 +35,7 @@ export const loggerMiddleware: RequestHandler = (req, res, next) => {
       },
       res: {
         code: statusCode,
+        ...(responseBody && { body: responseBody }),
       },
     });
   });
